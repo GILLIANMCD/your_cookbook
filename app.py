9812725dc9dcab19460ecb94ffba81b7584565ed
GILLIANMCD <gillian2677@gmail.com>
2019-08-17 18:12:46 +0000

    update test

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'cookbook_manager'
-app.config["MONGO_URI"] = ''
+app.config["MONGO_URI"] = 'mongodb+srv://Gillian:N3wdb@2019@myfirstcluster-eimbl.mongodb.net/cookbook_manager?retryWrites=true&w=majority'
 
 
 @app.route('/')
 def hello():
-    return 'Hello World ...two'
+    return 'Hello World ...three'
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '127.0.0.1'),
